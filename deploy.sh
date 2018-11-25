c80739495f4bb02a97ddc1544ddc854453969940
Timothy Crosley <timothy.crosley@gmail.com>
2018-11-25 13:42:24 -0800

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -32,10 +32,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../lovelicton_deploy.enc -out ../lovelicton_deploy -d
-chmod 600 ../lovelicton_deploy
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in lovelicton_deploy.enc -out lovelicton_deploy -d
+chmod 600 lovelicton_deploy
 eval `ssh-agent -s`
-ssh-add ../lovelicton_deploy
+ssh-add lovelicton_deploy
 
 # Now that we're all set up, we can push.
 ghp-import output && git push origin gh-pages
