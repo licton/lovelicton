06a608b512295f7505648536e579b1dfea7de2ac
Timothy Crosley <timothy.crosley@gmail.com>
2018-11-25 13:50:09 -0800

    Finish auto deploy

--- deploy.sh
+++ deploy.sh
@@ -20,23 +20,44 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO output
+cd output
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf output/**/* || exit 0
+
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
+cd output
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add -A .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt lovelicton_deploy.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in lovelicton_deploy.enc -out lovelicton_deploy -d
-chmod 600 lovelicton_deploy
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../lovelicton_deploy.enc -out ../lovelicton_deploy -d
+chmod 600 ../lovelicton_deploy
 eval `ssh-agent -s`
-ssh-add lovelicton_deploy
+ssh-add ../lovelicton_deploy
 
 # Now that we're all set up, we can push.
-ghp-import -s output
-git push origin gh-pages
+git push $SSH_REPO $TARGET_BRANCH
