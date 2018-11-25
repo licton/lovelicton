49b409718fbbb1d2c434498199039d5a7a142aac
Timothy Crosley <timothy.crosley@gmail.com>
2018-11-25 13:35:41 -0800

    Fix deploy key

--- deploy.sh
+++ deploy.sh
@@ -49,15 +49,15 @@ fi
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# Get the deploy key by using Travis's stored variables to decrypt lovelicton_deploy.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../lovelicton_deploy.enc -out ../lovelicton_deploy -d
+chmod 600 ../lovelicton_deploy
 eval `ssh-agent -s`
-ssh-add ../deploy_key
+ssh-add ../lovelicton_deploy
 
 # Now that we're all set up, we can push.
 ghp-import output && git push origin gh-pages
