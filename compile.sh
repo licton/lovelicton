2037a390d9c2e63cc59dd9f329399224e6964020
Timothy Crosley <timothy.crosley@gmail.com>
2018-11-25 13:33:24 -0800

    Attempt fix

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 rm -rf venv
-python -m venv venv
+python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 git clone https://github.com/timothycrosley/attila.git
